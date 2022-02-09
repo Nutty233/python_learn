3546c74581a93b75c204a2ed7ee173d537dcbc12
Nutty233 <89444595+Nutty233@users.noreply.github.com>
2022-02-09 18:16:52 +0900

    Add files via upload

--- Cluster Algorithm/main.py
+++ Cluster Algorithm/main.py
@@ -1,14 +1,20 @@
 import Open_Img
 import K_Means
 import cv2
+import datetime
 
 
 if __name__ == "__main__":
     img = Open_Img.Open_JPG_OpenCV(r"D:\Python_Learn\Cluster_Algorithm\001.jpg")
-    # dst = K_Means.K_Means_OpenCV(3, img)
-    # dst = K_Means.K_Means_sklearn(3, img)
-    dst, k = K_Means.K_Means_sklearn_Auto(2, 10, img)
-    print("k取",k,"时，k-means可以取得最好的聚类效果")
+    start_time = datetime.datetime.now()
+
+    # dst = K_Means.K_Means_OpenCV(5, img)
+    dst = K_Means.K_Means_sklearn(3, img)
+    # dst = K_Means.K_Means_sklearn_Auto(2, 10, img)
+
+    end_time = datetime.datetime.now()
+    print(end_time - start_time)
+
     cv2.imshow('img', dst)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
